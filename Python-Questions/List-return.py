aa5ca5252d26dbb1ba3f34ff0078d4714147c717
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-12-05 23:31:56 +0530

    New code

---
+++ Python-Questions/List-return.py
@@ -0,0 +1,31 @@
+# Use of List to print required Numbers only
+
+# a=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# newl=[]
+# for num in a:
+#     if num<10:
+#         newl.append(num)
+
+# print(newl)
+
+
+# # To input grad using If-Else
+# grade = input("Enter your grade: ")
+# if grade >= 90:
+#   print("A")
+# elif grade >= 80:
+#   print("B")
+# elif grade >= 70:
+#   print("C")
+# elif grade >= 65:
+#   print("D")
+# else:
+#   print("F")
+
+
+#List with only first and last input
+
+a=[12,34,67,90,54,78]
+
+newList=[a[0],a[len(a)-1]]
+print(newList)

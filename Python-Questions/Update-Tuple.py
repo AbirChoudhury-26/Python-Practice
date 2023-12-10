d5196b881cca021b4eb32f1c8e7870dc1e5017e0
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-12-10 21:09:32 +0530

    New

--- Python-Questions/Update-Tuple.py
+++ Python-Questions/Update-Tuple.py
@@ -1,11 +1,22 @@
-tl=[(1,2,3),(4,5,6),(7,8,9)]
+# tl=[(1,2,3),(4,5,6),(7,8,9)]
 
-print("The Original Tuple")
+# print("The Original Tuple")
 
-print(tl)
+# print(tl)
 
-print("After Modification")
-add=4
-res=[tuple(map(lambda ele: ele+add,i))for i in tl]
-print(res)
+# print("After Modification")
+# add=4
+# res=[tuple(map(lambda ele: ele+add,i))for i in tl]
+# print(res)
 
+#  Updating tuple usinf for loop
+def updates_tuple(tup,newval):
+    for i in range(len(tup)):
+        x,y,z=tup[i];
+        tup[i]=(newval,y,z)
+
+    return tup
+tup = [(1, 56, 'M'), (1, 14, 'F'), (2, 43, 'F'), (2, 10, 'M')]
+new_val=5
+
+print(updates_tuple(tup,new_val))

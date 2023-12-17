ded2c9b9fcc5e652ad08c728f54c5321a44d0a62
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-12-17 19:24:42 +0530

    new

---
+++ Python-Questions/Set-Remove-Methods.py
@@ -0,0 +1,10 @@
+# 1st Approach- Using Pop method
+
+def Remove(initial_set):
+	while initial_set:
+		initial_set.pop()
+		print(initial_set)
+
+
+initial_set = set([12, 10, 13, 15, 8, 9])
+Remove(initial_set)
--- Python-Questions/Set.py
+++ Python-Questions/Set.py
@@ -9,3 +9,12 @@ Set3 = {(1, "Lion"), ( 2, "Tiger"), (3, "Fox")}
 print("Size of Set1: " + str(sys.getsizeof(Set1)) + "bytes")
 print("Size of Set2: " + str(sys.getsizeof(Set2)) + "bytes")
 print("Size of Set3: " + str(sys.getsizeof(Set3)) + "bytes")
+
+# 2nd Approach
+
+# Creating a set using string
+test_set = set("geEks")
+
+# Iterating using enumerated for loop
+for id,val in enumerate(test_set):
+	print(id, val)

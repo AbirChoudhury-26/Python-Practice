d9298f3eb92649e51a90eeacc907859a8a1d977f
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-12-18 22:34:54 +0530

    New

--- Python-Questions/Creating-2D-Matrix.py
+++ Python-Questions/Creating-2D-Matrix.py
@@ -8,13 +8,16 @@
 
 # Creating a 1-D List array and Inserting values in it
 
-arr=[]
-rows, cols=5,5
-k=0
-for i in range(rows):
-	col = []
-	for j in range(cols):
-		col.append(k)
-	k=k+1
-	arr.append(col)
-print(arr)
+# arr=[]
+# rows, cols=5,5
+# k=0
+# for i in range(rows):
+# 	col = []
+# 	for j in range(cols):
+# 		col.append(k)
+# 	k=k+1
+# 	arr.append(col)
+# print(arr)
+
+# Creating a 2-D Array
+

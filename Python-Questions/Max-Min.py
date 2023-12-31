d22c3d4fab6708db20d6c51bb0ce7bb0520d8b50
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-12-31 18:36:09 +0530

    new

--- Python-Questions/Max-Min.py
+++ Python-Questions/Max-Min.py
@@ -12,3 +12,19 @@ for i in range(1, len(gfg_list)):
 		
 print('Minimum Element in the list', gfg_list, 'is', min_ele)
 print('Maximum Element in the list', gfg_list, 'is', max_ele)
+
+# # function to find minimum and maximum position in list
+def minimum(a, n):
+	# inbuilt function to find the position of minimum
+	minpos = a.index(min(a))
+
+	# inbuilt function to find the position of maximum
+	maxpos = a.index(max(a))
+
+	# printing the position
+	print("The maximum is at position", maxpos + 1)
+	print("The minimum is at position", minpos + 1)
+
+# driver code
+a = [3, 4, 1, 3, 4, 5]
+minimum(a, len(a))

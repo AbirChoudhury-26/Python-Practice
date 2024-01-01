73643bef75e2cd9508824adb0ee6887832d95165
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-01 20:10:17 +0530

    New

---
+++ Python-Questions/List-Comprehension.py
@@ -0,0 +1,12 @@
+# A Python list comprehension consists of brackets containing the expression, which is executed for each element along with the for loop to iterate over each element in the Python list. 
+
+# Example-1
+
+numbers = [12, 13, 14,] 
+doubled = [x *2 for x in numbers] 
+print(doubled)
+
+# Example-2 
+
+list = [i for i in range(11) if i % 2 == 0] 
+print(list)

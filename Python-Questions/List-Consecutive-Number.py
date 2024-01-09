61ec0b9b90cc2af0c762a19514933fbda5188c40
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-09 21:43:23 +0530

    New

---
+++ Python-Questions/List-Consecutive-Number.py
@@ -0,0 +1,11 @@
+# Approach #1 : using sorted() 
+
+# Python3 Program to Create list 
+# with integers within given range 
+
+def checkConsecutive(l):
+	return sorted(l) == list(range(min(l), max(l)+1))
+	
+# Driver Code
+lst = [2, 3, 1, 4, 5]
+print(checkConsecutive(lst))

07c2bb24ca6aa4cc2a12048af4e86e4ff08afbe7
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-11 22:59:57 +0530

    New

--- Python-Questions/Find-Largest.py
+++ Python-Questions/Find-Largest.py
@@ -9,3 +9,12 @@ list1.sort()
 
 # printing the last element
 print("Largest element is:", list1[-1])
+
+# Python program to find largest
+# number in a list
+
+# List of numbers
+list1 = [10, 20, 4, 45, 99]
+
+# Printing the maximum element
+print("Largest element is:", max(list1))

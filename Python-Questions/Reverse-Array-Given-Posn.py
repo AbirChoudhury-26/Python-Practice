fb291a11c1386647f8443499646f1ed7db269fe7
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-13 23:44:06 +0530

    New

---
+++ Python-Questions/Reverse-Array-Given-Posn.py
@@ -0,0 +1,10 @@
+# Using Slicing Method
+
+def reverseArrayUptoK(input, k):
+	print (input[k-1::-1] + input[k:])
+
+# Driver program
+if __name__ == "__main__":
+	input = [1, 2, 3, 4, 5, 6]
+	k = 4
+	reverseArrayUptoK(input, k)

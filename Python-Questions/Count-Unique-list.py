0f78f6dedefe1ba38f58f69a16f833efb35573ec
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-14 22:59:58 +0530

    New

--- Python-Questions/Count-Unique-list.py
+++ Python-Questions/Count-Unique-list.py
@@ -17,4 +17,14 @@ for item in input_list:
 print("No of unique items are:", count)
 
 
-#
+# Counter Method
+
+# importing Counter module
+from collections import Counter
+
+
+input_list = [1, 2, 2, 5, 8, 4, 4, 8]
+
+# creating a list with the keys
+items = Counter(input_list).keys()
+print("No of unique items in the list are:", len(items))

c227b485b60e28cecbf037663db8ae978f15488c
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-16 21:12:13 +0530

    New

--- Python-Questions/Max-in-Sublist.py
+++ Python-Questions/Max-in-Sublist.py
@@ -1,10 +1,20 @@
+# # Initialising List
+# a = [[10, 13, 454, 66, 44], [10, 8, 7, 23]]
+# lis = []
+
+# # find max in list
+# for p in a:
+# 	lis.append(max(p))
+
+# # Printing max
+# print(lis)
+
+
 # Initialising List
 a = [[10, 13, 454, 66, 44], [10, 8, 7, 23]]
-lis = []
 
 # find max in list
-for p in a:
-	lis.append(max(p))
+b = [max(p) for p in a]
 
 # Printing max
-print(lis)
+print(b)

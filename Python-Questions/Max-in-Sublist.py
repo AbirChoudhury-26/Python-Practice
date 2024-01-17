4048cf66244cee1ff0e1e49877c0657c137f057a
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-17 21:24:33 +0530

    New

--- Python-Questions/Max-in-Sublist.py
+++ Python-Questions/Max-in-Sublist.py
@@ -31,13 +31,19 @@
 # Get maximum number in each subist using Sort method
 
 # Initialising List
-a = [[10, 13, 454, 66, 44], [10, 8, 7, 23]]
-lis = []
+# a = [[10, 13, 454, 66, 44], [10, 8, 7, 23]]
+# lis = []
 
-# find max in list
-for p in a:
-	p.sort()
-	lis.append(p[-1])
+# # find max in list
+# for p in a:
+# 	p.sort()
+# 	lis.append(p[-1])
 
-# Printing max
-print(lis)
+# # Printing max
+# print(lis)
+
+# Max using MAP-Lambda Function
+
+# First define a list of sublists a. Then, use the map() function to apply a lambda function to each sublist in a. The lambda function simply finds the maximum value in the sublist using the built-in max() function.
+#Finally, we convert the result of the map() function to a list using the list() function, and assign it to the variable lis. Finally, we print the resulting list of maximum values
+

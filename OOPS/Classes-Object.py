2bd1706be3849e255029812edcaef2a2e4f4a301
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-21 23:53:03 +0530

    All Codes

--- OOPS/Classes-Object.py
+++ OOPS/Classes-Object.py
@@ -61,3 +61,17 @@ Rodger = Dog()
 # and method through objects
 print(Rodger.attr1)
 Rodger.fun()
+
+
+class GFG:
+	def __init__(somename, name, company):
+		somename.name = name
+		somename.company = company
+
+	def show(somename):
+		print("Hello my name is " + somename.name +
+			" and I work in "+somename.company+".")
+
+
+obj = GFG("John", "GeeksForGeeks")
+obj.show()

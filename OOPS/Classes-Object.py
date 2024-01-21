4f780d25c0f40b6233b9b7093b21c23e4e10f89b
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-21 23:28:29 +0530

    All Codes

--- OOPS/Classes-Object.py
+++ OOPS/Classes-Object.py
@@ -22,3 +22,42 @@ Rodger = Dog()
 # and method through objects
 print(Rodger.attr1)
 Rodger.fun()
+
+
+class GFG:
+	def __init__(self, name, company):
+		self.name = name
+		self.company = company
+
+	def show(self):
+		print("Hello my name is " + self.name+" and I" +
+			" work in "+self.company+".")
+
+
+obj = GFG("John", "GeeksForGeeks")
+obj.show()
+
+# Python3 program to
+# demonstrate instantiating
+# a class
+class Dog:
+
+	# A simple class
+	# attribute
+	attr1 = "mammal"
+	attr2 = "dog"
+
+	# A sample method
+	def fun(self):
+		print("I'm a", self.attr1)
+		print("I'm a", self.attr2)
+
+
+# Driver code
+# Object instantiation
+Rodger = Dog()
+
+# Accessing class attributes
+# and method through objects
+print(Rodger.attr1)
+Rodger.fun()

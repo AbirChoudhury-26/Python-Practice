1a914be4c664357f21fcaf0ad9332684929fcaf6
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-22 22:36:59 +0530

    New

--- OOPS/Classes-Object.py
+++ OOPS/Classes-Object.py
@@ -63,15 +63,34 @@ print(Rodger.attr1)
 Rodger.fun()
 
 
-class GFG:
-	def __init__(somename, name, company):
-		somename.name = name
-		somename.company = company
+# class GFG:
+# 	def __init__(somename, name, company):
+# 		somename.name = name
+# 		somename.company = company
 
-	def show(somename):
-		print("Hello my name is " + somename.name +
-			" and I work in "+somename.company+".")
+# 	def show(somename):
+# 		print("Hello my name is " + somename.name +
+# 			" and I work in "+somename.company+".")
 
 
-obj = GFG("John", "GeeksForGeeks")
-obj.show()
+# obj = GFG("John", "GeeksForGeeks")
+# obj.show()
+
+
+# __init__() method
+# The __init__ method is similar to constructors in C++ and Java. Constructors are used to initializing the object’s state. Like methods, a constructor also contains a collection of statements(i.e. instructions) that are executed at the time of Object creation. It runs as soon as an object of a class is instantiated. The method is useful to do any initialization you want to do with your object.
+
+# Sample class with init method
+class Person:
+
+	# init method or constructor
+	def __init__(self, name):
+		self.name = name
+
+	# Sample Method
+	def say_hi(self):
+		print('Hello, my name is', self.name)
+
+
+p = Person('Nikhil')
+p.say_hi()

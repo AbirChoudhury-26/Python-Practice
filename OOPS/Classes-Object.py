2888a259d4e414d574a5f2816b920b9dc721e05d
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-22 22:38:30 +0530

    New

--- OOPS/Classes-Object.py
+++ OOPS/Classes-Object.py
@@ -81,16 +81,59 @@ Rodger.fun()
 # The __init__ method is similar to constructors in C++ and Java. Constructors are used to initializing the object’s state. Like methods, a constructor also contains a collection of statements(i.e. instructions) that are executed at the time of Object creation. It runs as soon as an object of a class is instantiated. The method is useful to do any initialization you want to do with your object.
 
 # Sample class with init method
-class Person:
+# class Person:
 
-	# init method or constructor
-	def __init__(self, name):
-		self.name = name
+# 	# init method or constructor
+# 	def __init__(self, name):
+# 		self.name = name
 
-	# Sample Method
-	def say_hi(self):
-		print('Hello, my name is', self.name)
+# 	# Sample Method
+# 	def say_hi(self):
+# 		print('Hello, my name is', self.name)
 
 
-p = Person('Nikhil')
-p.say_hi()
+# p = Person('Nikhil')
+# p.say_hi()
+
+# Class and Instance Variables
+# Instance variables are for data, unique to each instance and class variables are for attributes and methods shared by all instances of the class. Instance variables are variables whose value is assigned inside a constructor or method with self whereas class variables are variables whose value is assigned in the class.
+	
+# Python3 program to show that the variables with a value
+# assigned in the class declaration, are class variables and
+# variables inside methods and constructors are instance
+# variables.
+
+# Class for Dog
+
+
+class Dog:
+
+	# Class Variable
+	animal = 'dog'
+
+	# The init method or constructor
+	def __init__(self, breed, color):
+
+		# Instance Variable
+		self.breed = breed
+		self.color = color
+
+
+# Objects of Dog class
+Rodger = Dog("Pug", "brown")
+Buzo = Dog("Bulldog", "black")
+
+print('Rodger details:')
+print('Rodger is a', Rodger.animal)
+print('Breed: ', Rodger.breed)
+print('Color: ', Rodger.color)
+
+print('\nBuzo details:')
+print('Buzo is a', Buzo.animal)
+print('Breed: ', Buzo.breed)
+print('Color: ', Buzo.color)
+
+# Class variables can be accessed using class
+# name also
+print("\nAccessing class variable using class name")
+print(Dog.animal)

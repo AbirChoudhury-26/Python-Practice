745809db5435eb6297cb4c12a1332451cd44cb92
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-23 23:34:58 +0530

    New

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -14,3 +14,16 @@ class Person(object):
 # Driver code
 emp = Person("Satyam", 102) # An Object of Person
 emp.Display()
+
+class Emp(Person):
+
+def Print(self):
+	print("Emp class called")
+	
+Emp_details = Emp("Mayank", 103)
+
+# calling parent class function
+Emp_details.Display()
+
+# Calling child class function
+Emp_details.Print()

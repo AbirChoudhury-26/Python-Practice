927fc60e07926951c1c5491566275c0ed4b945b3
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-25 22:42:29 +0530

    New

--- OOPS/super-function.py
+++ OOPS/super-function.py
@@ -4,26 +4,57 @@ A method from a parent class can be called in Python using the super() function.
 """
 
 
-# parent class
-class Person():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# # parent class
+# class Person():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def display(self):
-        print(self.name, self.age)
+#     def display(self):
+#         print(self.name, self.age)
 
-# child class
-class Student(Person):
-    def __init__(self, name, age):
-        self.sName = name
-        self.sAge = age
-        # inheriting the properties of parent class
-        super().__init__("Rahul", age)
+# # child class
+# class Student(Person):
+#     def __init__(self, name, age):
+#         self.sName = name
+#         self.sAge = age
+#         # inheriting the properties of parent class
+#         super().__init__("Rahul", age)
 
-    def displayInfo(self):
-        print(self.sName, self.sAge)
+#     def displayInfo(self):
+#         print(self.sName, self.sAge)
 
-obj = Student("Mayank", 23)
-obj.display()
-obj.displayInfo()
+# obj = Student("Mayank", 23)
+# obj.display()
+# obj.displayInfo()
+
+# Inheritance with super function 
+
+# code
+# A Python program to demonstrate inheritance
+
+class Person:
+
+	# Constructor
+	def __init__(self, name, id):
+		self.name = name
+		self.id = id
+
+	# To check if this person is an employee
+	def Display(self):
+		print(self.name, self.id)
+	
+
+class Emp(Person):
+	
+	def __init__(self, name, id):
+		self.name_ = name
+		super().__init__(name, id)
+
+	def Print(self):
+		print("Emp class called")
+
+Emp_details = Emp("Mayank", 103)
+
+# calling parent class function
+print(Emp_details.name_, Emp_details.name)

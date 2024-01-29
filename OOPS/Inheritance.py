1b9f34b083fd09d0cf340e55a691cd7b441543b2
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-29 23:32:32 +0530

    New

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -102,26 +102,80 @@
 # Adding Property Inheritance
 
 # parent class
-class Person():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Person():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def display(self):
-        print(self.name, self.age)
+#     def display(self):
+#         print(self.name, self.age)
 
-# child class
-class Student(Person):
-    def __init__(self, name, age, dob):
-        self.sName = name
-        self.sAge = age
-        self.dob = dob
-        # inheriting the properties of parent class
-        super().__init__("Rahul", age)
+# # child class
+# class Student(Person):
+#     def __init__(self, name, age, dob):
+#         self.sName = name
+#         self.sAge = age
+#         self.dob = dob
+#         # inheriting the properties of parent class
+#         super().__init__("Rahul", age)
 
-    def displayInfo(self):
-        print(self.sName, self.sAge, self.dob)
+#     def displayInfo(self):
+#         print(self.sName, self.sAge, self.dob)
 
-obj = Student("Mayank", 23, "16-03-2000")
-obj.display()
-obj.displayInfo()
+# obj = Student("Mayank", 23, "16-03-2000")
+# obj.display()
+# obj.displayInfo()
+
+
+# Multi-Level Inheritance
+
+# When we have a child and grandchild relationship. This means that a child class will inherit from its parent class, which in turn is inheriting from its parent class.
+
+# A Python program to demonstrate inheritance
+
+# Base or Super class. Note object in bracket.
+# (Generally, object is made ancestor of all classes)
+# In Python 3.x "class Person" is
+# equivalent to "class Person(object)"
+
+class Base(object):
+
+	# Constructor
+	def __init__(self, name):
+		self.name = name
+
+	# To get name
+	def getName(self):
+		return self.name
+
+
+# Inherited or Sub class (Note Person in bracket)
+class Child(Base):
+
+	# Constructor
+	def __init__(self, name, age):
+		Base.__init__(self, name)
+		self.age = age
+
+	# To get name
+	def getAge(self):
+		return self.age
+
+# Inherited or Sub class (Note Person in bracket)
+
+
+class GrandChild(Child):
+
+	# Constructor
+	def __init__(self, name, age, address):
+		Child.__init__(self, name, age)
+		self.address = address
+
+	# To get address
+	def getAddress(self):
+		return self.address
+
+
+# Driver code
+g = GrandChild("Geek1", 23, "Noida")
+print(g.getName(), g.getAge(), g.getAddress())

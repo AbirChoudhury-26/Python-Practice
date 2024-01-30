a95ae9976cd95e9ea534c9385d631224b34f1dc7
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-30 23:30:18 +0530

    Types

--- OOPS/Types of Inheritance.py
+++ OOPS/Types of Inheritance.py
@@ -34,32 +34,80 @@ When a class can be derived from more than one base class this type of inheritan
 # multiple inheritance
 
 # Base class1
-class Mother:
-	mothername = ""
+# class Mother:
+# 	mothername = ""
 
-	def mother(self):
-		print(self.mothername)
+# 	def mother(self):
+# 		print(self.mothername)
 
 # Base class2
 
 
-class Father:
-	fathername = ""
+# class Father:
+# 	fathername = ""
 
-	def father(self):
-		print(self.fathername)
+# 	def father(self):
+# 		print(self.fathername)
+
+# # Derived class
+
+
+# class Son(Mother, Father):
+# 	def parents(self):
+# 		print("Father :", self.fathername)
+# 		print("Mother :", self.mothername)
+
+
+# # Driver's code
+# s1 = Son()
+# s1.fathername = "RAM"
+# s1.mothername = "SITA"
+# s1.parents()
+
+# Multi-Level Inheritance
+
+"""
+In multilevel inheritance, features of the base class and the derived class are further inherited into the new derived class. This is similar to a relationship representing a child and a grandfather. 
+"""
+
+# Python program to demonstrate
+# multilevel inheritance
+
+# Base class
+
+
+class Grandfather:
+
+	def __init__(self, grandfathername):
+		self.grandfathername = grandfathername
+
+# Intermediate class
+
+
+class Father(Grandfather):
+	def __init__(self, fathername, grandfathername):
+		self.fathername = fathername
+
+		# invoking constructor of Grandfather class
+		Grandfather.__init__(self, grandfathername)
 
 # Derived class
 
 
-class Son(Mother, Father):
-	def parents(self):
-		print("Father :", self.fathername)
-		print("Mother :", self.mothername)
+class Son(Father):
+	def __init__(self, sonname, fathername, grandfathername):
+		self.sonname = sonname
+
+		# invoking constructor of Father class
+		Father.__init__(self, fathername, grandfathername)
+
+	def print_name(self):
+		print('Grandfather name :', self.grandfathername)
+		print("Father name :", self.fathername)
+		print("Son name :", self.sonname)
 
 
-# Driver's code
-s1 = Son()
-s1.fathername = "RAM"
-s1.mothername = "SITA"
-s1.parents()
+# Driver code
+s1 = Son('Prince', 'Rampal', 'Lal mani')
+print(s1.grandfathername)
+s1.print_name()

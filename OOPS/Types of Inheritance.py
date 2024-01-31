d912d84bd34793bb4a517870aca81d507c38e498
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-01-31 22:56:51 +0530

    New

--- OOPS/Types of Inheritance.py
+++ OOPS/Types of Inheritance.py
@@ -76,38 +76,73 @@ In multilevel inheritance, features of the base class and the derived class are 
 # Base class
 
 
-class Grandfather:
+# class Grandfather:
 
-	def __init__(self, grandfathername):
-		self.grandfathername = grandfathername
+# 	def __init__(self, grandfathername):
+# 		self.grandfathername = grandfathername
 
-# Intermediate class
+# # Intermediate class
 
 
-class Father(Grandfather):
-	def __init__(self, fathername, grandfathername):
-		self.fathername = fathername
+# class Father(Grandfather):
+# 	def __init__(self, fathername, grandfathername):
+# 		self.fathername = fathername
 
-		# invoking constructor of Grandfather class
-		Grandfather.__init__(self, grandfathername)
+# 		# invoking constructor of Grandfather class
+# 		Grandfather.__init__(self, grandfathername)
 
 # Derived class
 
 
-class Son(Father):
-	def __init__(self, sonname, fathername, grandfathername):
-		self.sonname = sonname
+# class Son(Father):
+# 	def __init__(self, sonname, fathername, grandfathername):
+# 		self.sonname = sonname
 
-		# invoking constructor of Father class
-		Father.__init__(self, fathername, grandfathername)
+# 		# invoking constructor of Father class
+# 		Father.__init__(self, fathername, grandfathername)
 
-	def print_name(self):
-		print('Grandfather name :', self.grandfathername)
-		print("Father name :", self.fathername)
-		print("Son name :", self.sonname)
+# 	def print_name(self):
+# 		print('Grandfather name :', self.grandfathername)
+# 		print("Father name :", self.fathername)
+# 		print("Son name :", self.sonname)
 
 
-# Driver code
-s1 = Son('Prince', 'Rampal', 'Lal mani')
-print(s1.grandfathername)
-s1.print_name()
+# # Driver code
+# s1 = Son('Prince', 'Rampal', 'Lal mani')
+# print(s1.grandfathername)
+# s1.print_name()
+
+
+# Hierarchial Inheritance
+
+# Python program to demonstrate
+# Hierarchical inheritance
+
+
+# Base class
+class Parent:
+	def func1(self):
+		print("This function is in parent class.")
+
+# Derived class1
+
+
+class Child1(Parent):
+	def func2(self):
+		print("This function is in child 1.")
+
+# Derivied class2
+
+
+class Child2(Parent):
+	def func3(self):
+		print("This function is in child 2.")
+
+
+# Driver's code
+object1 = Child1()
+object2 = Child2()
+object1.func1()
+object1.func2()
+object2.func1()
+object2.func3()

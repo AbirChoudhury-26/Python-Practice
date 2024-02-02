0c69c86a024505c05f8ae999fa0459720c5f50b5
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-02 22:45:17 +0530

    New

--- OOPS/Polymorphism.py
+++ OOPS/Polymorphism.py
@@ -38,9 +38,40 @@ Polymorphism simply means having many forms. For example, we need to determine i
 # A simple Python function to demonstrate 
 # Polymorphism
 
-def add(x, y, z = 0): 
-	return x + y+z
+# def add(x, y, z = 0): 
+# 	return x + y+z
 
-# Driver code 
-print(add(2, 3))
-print(add(2, 3, 4))
+# # Driver code 
+# print(add(2, 3))
+# print(add(2, 3, 4))
+
+
+
+# Polymorphism with class Methods
+
+class India():
+	def capital(self):
+		print("New Delhi is the capital of India.")
+
+	def language(self):
+		print("Hindi is the most widely spoken language of India.")
+
+	def type(self):
+		print("India is a developing country.")
+
+class USA():
+	def capital(self):
+		print("Washington, D.C. is the capital of USA.")
+
+	def language(self):
+		print("English is the primary language of USA.")
+
+	def type(self):
+		print("USA is a developed country.")
+
+obj_ind = India()
+obj_usa = USA()
+for country in (obj_ind, obj_usa):
+	country.capital()
+	country.language()
+	country.type()

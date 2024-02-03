e8d633decbce0f60ab74c3cf92855c2fd6ae2cd2
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-03 12:12:11 +0530

    New

--- OOPS/Polymorphism.py
+++ OOPS/Polymorphism.py
@@ -123,33 +123,53 @@ for country in (obj_ind, obj_usa):
 # func(obj_usa)
 
 
-class India():
-	def capital(self):
-		print("New Delhi is the capital of India.")
+# class India():
+# 	def capital(self):
+# 		print("New Delhi is the capital of India.")
 
-	def language(self):
-		print("Hindi is the most widely spoken language of India.")
+# 	def language(self):
+# 		print("Hindi is the most widely spoken language of India.")
 
-	def type(self):
-		print("India is a developing country.")
+# 	def type(self):
+# 		print("India is a developing country.")
 
-class USA():
-	def capital(self):
-		print("Washington, D.C. is the capital of USA.")
+# class USA():
+# 	def capital(self):
+# 		print("Washington, D.C. is the capital of USA.")
 
-	def language(self):
-		print("English is the primary language of USA.")
+# 	def language(self):
+# 		print("English is the primary language of USA.")
 
-	def type(self):
-		print("USA is a developed country.")
+# 	def type(self):
+# 		print("USA is a developed country.")
 
-def func(obj):
-	obj.capital()
-	obj.language()
-	obj.type()
+# def func(obj):
+# 	obj.capital()
+# 	obj.language()
+# 	obj.type()
 
-obj_ind = India()
-obj_usa = USA()
+# obj_ind = India()
+# obj_usa = USA()
 
-func(obj_ind)
-func(obj_usa)
+# func(obj_ind)
+# func(obj_usa)
+
+
+class Animal:
+	def speak(self):
+		raise NotImplementedError("Subclass must implement this method")
+
+class Dog(Animal):
+	def speak(self):
+		return "Woof!"
+
+class Cat(Animal):
+	def speak(self):
+		return "Meow!"
+
+# Create a list of Animal objects
+animals = [Dog(), Cat()]
+
+# Call the speak method on each object
+for animal in animals:
+	print(animal.speak())

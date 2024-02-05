482e0ba253c54cdeef04d89bdbd393b590db57d0
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-05 22:46:46 +0530

    New

--- Python-Questions/File-Handling.py
+++ Python-Questions/File-Handling.py
@@ -25,3 +25,11 @@ File_object = open(r"File_Name", "Access_Mode")
 # # store its reference in the variable file1 
 # # and "MyFile2.txt" in D:\Text in file2 
 # file2 = open(r"D:\Text\MyFile2.txt", "w+") 
+
+
+# CLOSING a file
+
+# Opening and Closing a file "MyFile.txt" 
+# for object name file1. 
+# file1 = open("MyFile.txt", "w") 
+# file1.close() 

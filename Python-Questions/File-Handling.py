2cfedd8f92466d5230534bc967241e4ff9675643
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-05 22:44:18 +0530

    New

---
+++ Python-Questions/File-Handling.py
@@ -0,0 +1,27 @@
+# # Python program to demonstrate
+# # opening a file
+
+
+# # Open function to open the file "myfile.txt"
+# # (same directory) in read mode and store
+# # it's reference in the variable file1
+
+# file1 = open("myfile1.txt")
+
+# # Reading from file
+# print(file1.read())
+
+# file1.close()
+
+
+# WRITING  a file
+"""
+File_object = open(r"File_Name", "Access_Mode")
+"""
+# Open function to open the file "MyFile1.txt" 
+# (same directory) in read mode and 
+# file1 = open("MyFile.txt", "w") 
+
+# # store its reference in the variable file1 
+# # and "MyFile2.txt" in D:\Text in file2 
+# file2 = open(r"D:\Text\MyFile2.txt", "w+") 

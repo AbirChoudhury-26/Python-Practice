9a0cbfdecf9a8e5db70a89a25ea7c1cc9fb1c3ac
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-07 20:54:35 +0530

    New

--- Python-Questions/File-Handling.py
+++ Python-Questions/File-Handling.py
@@ -88,3 +88,20 @@ File_object = open(r"File_Name", "Access_Mode")
 # print(file1.read())
 # print()
 # file1.close()
+
+
+# Program to show various ways to
+# write data to a file using with statement
+
+# L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+
+# # Writing to file
+# with open("myfile.txt", "w") as file1:
+# 	# Writing data to a file
+# 	file1.write("Hello \n")
+# 	file1.writelines(L)
+
+# # Reading from file
+# with open("myfile.txt", "r+") as file1:
+# 	# Reading form a file
+# 	print(file1.read())

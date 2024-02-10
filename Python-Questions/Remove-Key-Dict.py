bce163b7edc5be286db0ec418f6035bf70ea81c8
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-10 23:43:51 +0530

    Python basics

--- Python-Questions/Remove-Key-Dict.py
+++ Python-Questions/Remove-Key-Dict.py
@@ -1,16 +1,42 @@
+# # Initializing dictionary
+# test_dict = {"Arushi": 22, "Mani": 21, "Haritha": 21}
+
+# # Printing dictionary before removal
+# print("The dictionary before performing remove is : ", test_dict)
+
+# # Using del to remove a dict
+# # removes Mani
+# del test_dict['Mani']
+
+# # Printing dictionary after removal
+# print("The dictionary after remove is : ", test_dict)
+
+# # Using del to remove a dict
+# # raises exception
+# del test_dict['Mani']
+
+
 # Initializing dictionary
-test_dict = {"Arushi": 22, "Mani": 21, "Haritha": 21}
+test_dict = {"Arushi": 22, "Anuradha": 21, "Mani": 21, "Haritha": 21}
 
 # Printing dictionary before removal
-print("The dictionary before performing remove is : ", test_dict)
+print("The dictionary before performing remove is : " + str(test_dict))
 
-# Using del to remove a dict
+# Using pop() to remove a dict. pair
 # removes Mani
-del test_dict['Mani']
+removed_value = test_dict.pop('Mani')
 
 # Printing dictionary after removal
-print("The dictionary after remove is : ", test_dict)
+print("The dictionary after remove is : " + str(test_dict))
+print("The removed key's value is : " + str(removed_value))
 
-# Using del to remove a dict
-# raises exception
-del test_dict['Mani']
+print('\r')
+
+# Using pop() to remove a dict. pair
+# doesn't raise exception
+# assigns 'No Key found' to removed_value
+removed_value = test_dict.pop('Manjeet', 'No Key found')
+
+# Printing dictionary after removal
+print("The dictionary after remove is : " + str(test_dict))
+print("The removed key's value is : " + str(removed_value))

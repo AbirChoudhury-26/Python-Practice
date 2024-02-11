5f25b23997e2c73729876ee0fc0c67b5df4003b0
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-11 22:11:47 +0530

    New

--- Python-Questions/Merge-2-Dict.py
+++ Python-Questions/Merge-2-Dict.py
@@ -15,4 +15,29 @@ dict2 = {'a': 6, 'b': 4}
 dict3 = Merge(dict1, dict2) 
 print(dict3)
 
-# This code is contributed by virentanti16
+
+
+# Using Keys and Loops
+
+"""
+For each key:
+If the key already exists in dict1, update the value in dict1 with the value from dict2.
+If the key does not exist in dict1, add the key-value pair from dict2 to dict1.
+Return the modified dict1.
+"""
+
+# code
+# Python code to merge dictionary
+def Merge(dict1, dict2):
+	for i in dict2.keys():
+		dict1[i]=dict2[i]
+	return dict1
+	
+# Driver code
+dict1 = {'x': 10, 'y': 8}
+dict2 = {'a': 6, 'b': 4}
+dict3 = Merge(dict1, dict2)
+print(dict3)
+
+# This code is contributed by Bhavya Koganti
+

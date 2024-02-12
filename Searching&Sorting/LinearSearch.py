01a84319d3953a56d8d1807cd2be34d4ccf08621
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-12 23:08:59 +0530

    New

---
+++ Searching&Sorting/LinearSearch.py
@@ -0,0 +1,15 @@
+def search(arr, x):
+ 
+    for i in range(len(arr)):
+ 
+        if arr[i] == x:
+            return i
+ 
+    return -1
+
+arr= [10, 20, 80, 30, 60, 50,  110, 100, 130, 170]
+x = 110
+
+ans=search(arr,x)
+
+print(f"Element is present at index {ans}")

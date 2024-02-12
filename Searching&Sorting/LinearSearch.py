bd57571f08cf493069407a5cd37ea9ac5a2c5cb3
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-12 23:09:45 +0530

    New

--- Searching&Sorting/LinearSearch.py
+++ Searching&Sorting/LinearSearch.py
@@ -1,15 +1,22 @@
-def search(arr, x):
+# def search(arr, x):
  
-    for i in range(len(arr)):
+#     for i in range(len(arr)):
  
-        if arr[i] == x:
-            return i
+#         if arr[i] == x:
+#             return i
  
-    return -1
+#     return -1
 
-arr= [10, 20, 80, 30, 60, 50,  110, 100, 130, 170]
-x = 110
+# arr= [10, 20, 80, 30, 60, 50,  110, 100, 130, 170]
+# x = 110
 
-ans=search(arr,x)
+# ans=search(arr,x)
 
-print(f"Element is present at index {ans}")
+# print(f"Element is present at index {ans}")
+
+def search(arr, curr_index, key):
+    if curr_index == -1:
+        return -1
+    if arr[curr_index] == key:
+        return curr_index
+    return search(arr, curr_index-1, key)

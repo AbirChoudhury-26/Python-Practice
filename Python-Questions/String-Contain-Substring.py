77d7b3d3dca0598ad789c56d30fa7309eac1f506
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-18 18:28:11 +0530

    Substring Method-2

--- Python-Questions/String-Contain-Substring.py
+++ Python-Questions/String-Contain-Substring.py
@@ -2,10 +2,32 @@
 
 """Using an “in” operator which is used as a comparison operator"""
 
-# Take input from users
-MyString1 = "A geek in need is a geek indeed"
-reqString="ned"
-if reqString in MyString1:
-	print("Yes! it is present in the string")
+
+# MyString1 = "A geek in need is a geek indeed"
+# reqString="ned"
+# if reqString in MyString1:
+# 	print("Yes! it is present in the string")
+# else:
+# 	print("No! it is not present")
+ 
+
+
+ # Method- 2
+
+"""
+Split Method-  First split the given string into words and store them in a variable s then using the if condition, check if a substring is present in the given string or not.
+"""
+
+# input strings str1 and substr
+string = "geeks for geeks" # or string=input() -> taking input from the user
+substring = "geeks" # or substring=input()
+
+# splitting words in a given string
+s = string.split()
+print(s)  # This returns a list of substrings of original String
+# checking condition
+# if substring is present in the given string then it gives output as yes
+if substring in s:
+	print("yes")
 else:
-	print("No! it is not present")
+	print("no")

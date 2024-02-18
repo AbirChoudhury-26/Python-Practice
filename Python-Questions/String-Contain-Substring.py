25708fccb8c69dcfcbb80d301c11536012e962b3
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-18 18:06:59 +0530

    Substring

---
+++ Python-Questions/String-Contain-Substring.py
@@ -0,0 +1,11 @@
+# Method-1
+
+"""Using an “in” operator which is used as a comparison operator"""
+
+# Take input from users
+MyString1 = "A geek in need is a geek indeed"
+reqString="ned"
+if reqString in MyString1:
+	print("Yes! it is present in the string")
+else:
+	print("No! it is not present")

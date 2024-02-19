59718e5285278b26c9d5008e2e7a853814dbb746
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-19 22:56:41 +0530

    New

--- Python-Questions/Split-Methods.py
+++ Python-Questions/Split-Methods.py
@@ -14,3 +14,15 @@ Separator: This is a delimiter. The string splits at this specified separator. I
 Maxsplit: It is a number, that tells us to split the string into a maximum of the provided number of times. If it is not provided then the default is -1 which means there is no limit.
 
 """
+
+
+word = 'geeks, for, geeks, pawan'
+
+# maxsplit: 0
+print(word.split(', ', 0))
+
+# maxsplit: 4
+print(word.split(', ', 4))
+
+# maxsplit: 1
+print(word.split(', ', 1))

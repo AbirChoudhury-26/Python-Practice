3482d09399aed7cd1f86564fdc5e96a7bc6e3529
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-21 23:10:58 +0530

    New

--- Python-Questions/Word-Frequency-Char.py
+++ Python-Questions/Word-Frequency-Char.py
@@ -1,16 +1,45 @@
+# Method-1 
+
+""" Using Dictionary Comprehnesion +count()"""
+
+# # Python3 code to demonstrate working of
+# # Words Frequency in String Shorthands
+# # Using dictionary comprehension + count() + split()
+
+# # Initializing string
+# test_str = 'Gfg is best . Geeks are good and Geeks like Gfg'
+
+# # Printing original string
+# print("The original string is : " + str(test_str))
+
+# # Words Frequency in String Shorthands
+# # Using dictionary comprehension + count() + split()
+# res = {key: test_str.count(key) for key in test_str.split()}
+
+# # Printing result
+# print("The words frequency : " + str(res))
+
+
+# Method-2
+
+""" Using Counter + split"""
+
 # Python3 code to demonstrate working of
 # Words Frequency in String Shorthands
-# Using dictionary comprehension + count() + split()
+# Using Counter() + split()
 
-# Initializing string
+from collections import Counter
+
+# initializing string
 test_str = 'Gfg is best . Geeks are good and Geeks like Gfg'
 
-# Printing original string
+# printing original string
 print("The original string is : " + str(test_str))
 
 # Words Frequency in String Shorthands
-# Using dictionary comprehension + count() + split()
-res = {key: test_str.count(key) for key in test_str.split()}
+# using Counter() + split()
+res = Counter(test_str.split())
 
 # Printing result
-print("The words frequency : " + str(res))
+print("The words frequency : " + str(dict(res)))
+

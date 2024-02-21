1d65df46fea9184d74a16f4a97a00efcf27450b4
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-21 23:12:13 +0530

    New

--- Python-Questions/Word-Frequency-Char.py
+++ Python-Questions/Word-Frequency-Char.py
@@ -43,3 +43,28 @@ res = Counter(test_str.split())
 # Printing result
 print("The words frequency : " + str(dict(res)))
 
+
+# Method-3
+
+""" Using dictionary comprehension + operator.countOf() + split() :"""
+
+# Python3 code to demonstrate working of
+# Words Frequency in String Shorthands
+# Using dictionary comprehension + operator.countOf() + split()
+
+import operator as op
+
+# Initializing string
+test_str = 'Gfg is best . Geeks are good and Geeks like Gfg'
+
+# Printing original string
+print("The original string is : " + str(test_str))
+listString = test_str.split()
+
+# Words Frequency in String Shorthands
+# Using dictionary comprehension + operator.countOf()
+res = {key: op.countOf(listString, key) for key in listString}
+
+# Printing the result
+print("The words frequency : " + str(res))
+

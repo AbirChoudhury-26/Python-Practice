e594a8707d6182183a50c50dd8c6f6a8464b6da7
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-22 23:21:10 +0530

    New

--- Python-Questions/Cube-Tuple.py
+++ Python-Questions/Cube-Tuple.py
@@ -29,3 +29,23 @@ res = [(val, val**3) for val in list1]
 # print the result
 print(res)
 
+
+
+# Method-3
+
+
+# creating a list
+list1 = [1, 2, 5, 6]
+
+# creating an empty list to store the result
+res = []
+
+# iterating through each value in the list
+for val in list1:
+	# creating a tuple of the value and its cube
+	tup = (val, val**3)
+	# adding the tuple to the result list
+	res.append(tup)
+
+# print the result
+print(res)

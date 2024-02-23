adfc971cea1748c00f72f15b6d2b400ceebd99b2
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-23 23:05:49 +0530

    New

--- Python-Questions/List-Slicing.py
+++ Python-Questions/List-Slicing.py
@@ -1,5 +1,23 @@
+# # Initialize list
+# List = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# # Show original list
+# print("Original List:\n", List)
+
+# print("\nSliced Lists: ")
+
+# # Display sliced list
+# print(List[3:9:2])
+
+# # Display sliced list
+# print(List[::2])
+
+# # Display sliced list
+# print(List[::])
+
+
 # Initialize list
-List = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+List = [-999, 'G4G', 1706256, '^_^', 3.1496]
 
 # Show original list
 print("Original List:\n", List)
@@ -7,10 +25,13 @@ print("Original List:\n", List)
 print("\nSliced Lists: ")
 
 # Display sliced list
-print(List[3:9:2])
+print(List[10::2])
 
 # Display sliced list
-print(List[::2])
+print(List[1:1:1])
 
 # Display sliced list
-print(List[::])
+print(List[-1:-1:-1])
+
+# Display sliced list
+print(List[:0:])

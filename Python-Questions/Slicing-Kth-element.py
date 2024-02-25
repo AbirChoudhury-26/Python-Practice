277e069f128217c47a8e6e1fb245c355dc2038a3
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-25 21:47:36 +0530

    New Method

--- Python-Questions/Slicing-Kth-element.py
+++ Python-Questions/Slicing-Kth-element.py
@@ -64,4 +64,30 @@ K = 2
 res = list(islice(test_list, K, None))
 
 print(res) # [2, 3, 9]
-#This code is contributed by Edula Vinay Kumar Reddy
+
+
+# Method-4
+
+""" Using a for loop to iterate over the list and append elements to a new list starting from the K index."""
+
+
+# Python3 code to demonstrate 
+# list slicing from K to end
+# without specifying last element 
+
+# initializing list
+test_list = [5, 6, 2, 3, 9]
+
+# printing original list
+print ("The original list is : " + str(test_list))
+
+# index to begin slicing
+K = 2
+
+# using for loop to slice list from K to end
+res = []
+for i in range(K, len(test_list)):
+	res.append(test_list[i])
+
+# printing result 
+print ("The sliced list is : " + str(res))

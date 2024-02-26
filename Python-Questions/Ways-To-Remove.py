b09cf2becc46272235dc0124979d448af9a047ab
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-26 23:00:46 +0530

    New

--- Python-Questions/Ways-To-Remove.py
+++ Python-Questions/Ways-To-Remove.py
@@ -1,6 +1,26 @@
+# # Python code to demonstrate
+# # element removal in list
+# # using remove() method
+
+# test_list1 = [1, 3, 4, 6, 3]
+# test_list2 = [1, 4, 5, 4, 5]
+
+# # Printing initial list
+# print("The list before element removal is : "
+# 	+ str(test_list1))
+
+# # using remove() to remove list element3
+# test_list1.remove(3)
+
+# # Printing list after removal
+# # only first occurrence deleted
+# print("The list after element removal is : "
+# 	+ str(test_list1))
+
+
 # Python code to demonstrate
 # element removal in list
-# using remove() method
+# using filter() + Lambda function
 
 test_list1 = [1, 3, 4, 6, 3]
 test_list2 = [1, 4, 5, 4, 5]
@@ -9,10 +29,10 @@ test_list2 = [1, 4, 5, 4, 5]
 print("The list before element removal is : "
 	+ str(test_list1))
 
-# using remove() to remove list element3
-test_list1.remove(3)
+# using filter() + Lambda function
+# to remove list element 3
+test_list1 = list(filter(lambda x: x != 3, test_list1))
 
 # Printing list after removal
-# only first occurrence deleted
 print("The list after element removal is : "
 	+ str(test_list1))

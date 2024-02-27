a85b9f3e0d6088c5755ffc38ac1f7781813c486f
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-02-27 21:51:38 +0530

    New

--- Python-Questions/Ways-To-Remove.py
+++ Python-Questions/Ways-To-Remove.py
@@ -1,3 +1,5 @@
+# Method-1 :  Using remove() 
+
 # # Python code to demonstrate
 # # element removal in list
 # # using remove() method
@@ -18,6 +20,33 @@
 # 	+ str(test_list1))
 
 
+# Method-2 :  Using set.disard()
+
+# Python code to demonstrate
+# element removal in list
+# using discard() method
+
+test_list1 = [1, 3, 4, 6, 3]
+test_list2 = [1, 4, 5, 4, 5]
+
+# Printing initial list
+print("The list before element removal is : "
+	+ str(test_list2))
+
+# using discard() to remove list element 4
+test_list2 = set(test_list2)
+test_list2.discard(4)
+
+test_list2 = list(test_list2)
+
+# Printing list after removal
+# removes element as distinct initially
+print("The list after element removal is : "
+	+ str(test_list2))
+
+
+# Method-3 : Using Lambda Method
+
 # Python code to demonstrate
 # element removal in list
 # using filter() + Lambda function
@@ -37,6 +66,7 @@
 # print("The list after element removal is : "
 # 	+ str(test_list1))
 
+# Method-4 : Using List in Comprehension
 
 # Python code to demonstrate
 # element removal in list
@@ -57,3 +87,5 @@ test_list2 = [x for x in test_list2 if x != 4]
 # Printing list after removal
 print("The list after element removal is : "
 	+ str(test_list2))
+
+

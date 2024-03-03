8d02c6217e8140cfd05932adfe26c71803ab9950
AbirChoudhury-26 <abirudaipur@gmail.com>
2024-03-03 23:31:23 +0530

    New

--- Python-Questions/Reverse-List.py
+++ Python-Questions/Reverse-List.py
@@ -94,3 +94,14 @@ for i in lst:
 # printing result
 print(l)
 
+# Method-6
+
+""" Using List Comprehension"""
+
+original_list = [10, 11, 12, 13, 14, 15]
+new_list = [original_list[len(original_list) - i]
+			for i in range(1, len(original_list)+1)]
+print(new_list)
+
+
+
